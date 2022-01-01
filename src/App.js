2ecbda126a425ca32e96e66e003bcd22743a16d2
Darshan Tarsariya <darshantarsariya17@gmail.com>
2022-01-01 14:56:40 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -2,45 +2,34 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
 
 
+
+
 import Course from "./components/course/Course";
-// import Login from "./components/login/login";
-// import SignUp from "./components/login/signup";
-import Assignment from './components/assignment/assignment';
+import Login from "./components/login/login";
+import SignUp from "./components/login/signup";
+import Assignment from './components/assignment/Assignment';
 
 function App() {
   return (
     <Router>    
-    <div className="App m-5 mb-5" >
-      <nav className="navbar   fixed-top">
-        <div className="container">
-          
-        
-            <ul className="navbar-nav ">
-             
-              <li className="nav-item">
-                <Link className="nav-link" to={"/course"}><button type="button" class="btn btn-primary">Course</button></Link>
-              </li>
-            
-            </ul>
-          
-        </div>
-      </nav>
+    <div className="App " >
+     
 
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Switch>
-            {/* <Route exact path='/' component={Login} />
+            <Route exact path='/' component={Login} />
             <Route path="/sign-in" component={Login} />
             <Route path="/sign-up" component={SignUp} />
-            <Route path="/course" component={Course} />*/}
+            <Route path="/course" component={Course} />
             <Route path="/assignment" component={Assignment} /> 
-            <Route path="/" component={Course} />
+            <Route path="/course" component={Course} />
           </Switch>
           
         </div>
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,33 +1,59 @@
-import React, { Component } from "react";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-export default class Login extends Component {
-    render() {
-        return (
-            <form class="form-group col-md-6 mt-5">
-                <h3>Log In</h3>
+const Login = () => {
+  const history = useHistory();
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      history.push('/course')
+  }
+  return (
+    <div className="container">
+      <form class="form-group col-md-6 mt-5" onSubmit={handleSubmit}>
+        <h3>Log In</h3>
 
-                <div className="form-group mb-3">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+        <div className="form-group mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
 
-                <div className="form-group mb-3">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+        <div className="form-group mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
 
-                <div className="form-group mb-3">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
+        <div className="form-group mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
 
-                <button type="submit" className="btn btn-primary btn-block mb-3">Submit</button>
-                <p className="forgot-password text-right mb-3">
-                    Forgot <a href=" #">password?</a>
-                </p>
-            </form>
-        );
-    }
-}
+        <a href="/course">
+          <button type="submit" className="btn btn-primary btn-block mb-3">
+            Submit
+          </button>
+        </a>
+        <p className="forgot-password text-right mb-3">
+          New User ? <a href="/signup">Sign Up Here</a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
